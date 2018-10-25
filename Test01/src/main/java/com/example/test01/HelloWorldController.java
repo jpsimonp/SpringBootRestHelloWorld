98595ddd39cad26e114bfdbb376c4c8a468f5933
jpsimon <jpsimon@jpsimon10.indra.es>
2018-10-25 14:38:24 +0200

    First version

---
+++ Test01/src/main/java/com/example/test01/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.test01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	private static final String template = "Hello, %s!";
+
+	/*
+	 * http://localhost:8080/greeting?name=Juan_Pablo
+	 * http://localhost:8080/greeting
+	 */
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	System.out.println("Received greeting call, name: '" + name + "'!!!");
+        return String.format(template, name);
+    }
+
+}
